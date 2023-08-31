44e6cb5c760f12e9dd2a71a48c4c0c8ce4de8ee0
Anthony DelVecchio <amdelvecchio1225@gmail.com>
2023-08-31 13:29:34 -0400

    section 3 progress

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({setToken}) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
 
     async function handleSubmit(event) {
+
         event.preventDefault();
         try {
             const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {method: "POST"});
             const result = response.json;
-            console.log(result);
+            setToken(result.token);
         } catch(error) {
             setError(error.message);
         }
